b0a02b9c4d4a91802792aedea3f2803dcaba0fd8
Anatoliy <leaved@abv.bg>
2022-05-24 11:52:42 +0300

    add view all members and add members actions into dashboard controller with views.

--- AidSystem/Controllers/Dashboard/DashboardController.cs
+++ AidSystem/Controllers/Dashboard/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AidSystemBLL.BLL;
+using CharitySystemBLL.BLL;
+using CharitySystemBLL.BLL.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CharitySystem.Controllers.Dashboard
 {
@@ -8,7 +11,44 @@ namespace CharitySystem.Controllers.Dashboard
         {
             CharitySystemBLL.BLL.BLO.Dashboard.Dashboard dashboard = new CharitySystemBLL.BLL.BLO.Dashboard.Dashboard();
             return View(dashboard.GetViewModels());
-            //return View();
+        }
+
+        public IActionResult ViewAllMembers()
+        {
+            MemberBLL memberBLL = new MemberBLL();
+            return View(memberBLL.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult AddMember()
+        {
+            OrganizationBLL organizationBLL = new OrganizationBLL();
+            MemberOrganizationDTO memberOrganizationDTO = new MemberOrganizationDTO();
+            memberOrganizationDTO.Organizations = organizationBLL.GetAll().ToList();
+            return View(memberOrganizationDTO);
+        }
+
+        [HttpPost]
+        public IActionResult AddMember(
+            string FirstName,
+            string SecondName,
+            string LastName,
+            int Code,
+            int OrganizationID)
+        {
+            MemberBLL member = new MemberBLL();
+            {
+                if (FirstName != null)
+                {
+                    member.Add(
+                 FirstName,
+                 SecondName,
+                 LastName,
+                 Code,
+                 OrganizationID);
+                }
+            }
+            return RedirectToAction("Dashboard", "Dashboard");
         }
     }
 }
--- AidSystem/Controllers/MemberController.cs
+++ AidSystem/Controllers/MemberController.cs
@@ -15,43 +15,38 @@ namespace AidSystem.Controllers
         {
             return View();
         }
-        public IActionResult ViewAllMembers()
-        {
-            MemberBLL memberBLL = new MemberBLL();
-            return View(memberBLL.GetAll());
-        }
 
-        [HttpGet]
-        public IActionResult AddMember()
-        {
-            OrganizationBLL organizationBLL = new OrganizationBLL();
-            MemberOrganizationDTO memberOrganizationDTO = new MemberOrganizationDTO();
-            memberOrganizationDTO.Organizations = organizationBLL.GetAll().ToList();
-            return View(memberOrganizationDTO);
-        }
+        //[HttpGet]
+        //public IActionResult AddMember()
+        //{
+        //    OrganizationBLL organizationBLL = new OrganizationBLL();
+        //    MemberOrganizationDTO memberOrganizationDTO = new MemberOrganizationDTO();
+        //    memberOrganizationDTO.Organizations = organizationBLL.GetAll().ToList();
+        //    return View(memberOrganizationDTO);
+        //}
 
-        [HttpPost]
-        public IActionResult AddMember(
-            string FirstName,
-            string SecondName,
-            string LastName,
-            int Code,
-            int OrganizationID)
-        {
-            MemberBLL member = new MemberBLL();
-            {
-                if (FirstName != null)
-                {
-                   member.Add(
-                FirstName,
-                SecondName,
-                LastName,
-                Code,
-                OrganizationID);
-                }
-            }
-            return RedirectToAction("Index", "Home");
-        }
+        //[HttpPost]
+        //public IActionResult AddMember(
+        //    string FirstName,
+        //    string SecondName,
+        //    string LastName,
+        //    int Code,
+        //    int OrganizationID)
+        //{
+        //    MemberBLL member = new MemberBLL();
+        //    {
+        //        if (FirstName != null)
+        //        {
+        //           member.Add(
+        //        FirstName,
+        //        SecondName,
+        //        LastName,
+        //        Code,
+        //        OrganizationID);
+        //        }
+        //    }
+        //    return RedirectToAction("Index", "Home");
+        //}
         
         public IActionResult EditMember(int ID)
         {
