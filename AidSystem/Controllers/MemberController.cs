271e69bb289edd8c608b64ed5d2073da2d668cdd
Anatoliy <leaved@abv.bg>
2022-05-26 12:16:11 +0300

    Code refactoring,move back member crud operations to member controller,make 3 partial views for spinner,sidebar and navbar,delete old views in dashboard for members

--- AidSystem/Controllers/MemberController.cs
+++ AidSystem/Controllers/MemberController.cs
@@ -16,50 +16,53 @@ namespace AidSystem.Controllers
             return View();
         }
 
-        //[HttpGet]
-        //public IActionResult AddMember()
-        //{
-        //    OrganizationBLL organizationBLL = new OrganizationBLL();
-        //    MemberOrganizationDTO memberOrganizationDTO = new MemberOrganizationDTO();
-        //    memberOrganizationDTO.Organizations = organizationBLL.GetAll().ToList();
-        //    return View(memberOrganizationDTO);
-        //}
+        public IActionResult ViewAllMembers()
+        {
+            MemberBLL memberBLL = new MemberBLL();
+            return View(memberBLL.GetAll());
+        }
 
-        //[HttpPost]
-        //public IActionResult AddMember(
-        //    string FirstName,
-        //    string SecondName,
-        //    string LastName,
-        //    int Code,
-        //    int OrganizationID)
-        //{
-        //    MemberBLL member = new MemberBLL();
-        //    {
-        //        if (FirstName != null)
-        //        {
-        //           member.Add(
-        //        FirstName,
-        //        SecondName,
-        //        LastName,
-        //        Code,
-        //        OrganizationID);
-        //        }
-        //    }
-        //    return RedirectToAction("Index", "Home");
-        //}
-        
+        [HttpGet]
+        public IActionResult AddMember()
+        {
+            OrganizationBLL organizationBLL = new OrganizationBLL();
+            MemberOrganizationDTO memberOrganizationDTO = new MemberOrganizationDTO();
+            memberOrganizationDTO.Organizations = organizationBLL.GetAll().ToList();
+            return View(memberOrganizationDTO);
+        }
+
+        [HttpPost]
+        public IActionResult AddMember(
+            string FirstName,
+            string SecondName,
+            string LastName,
+            int Code,
+            int OrganizationID)
+        {
+            MemberBLL member = new MemberBLL();
+            {
+                if (FirstName != null)
+                {
+                    member.Add(
+                 FirstName,
+                 SecondName,
+                 LastName,
+                 Code,
+                 OrganizationID);
+                }
+            }
+            return RedirectToAction("Dashboard", "Dashboard");
+        }
+
+        [HttpGet]
         public IActionResult EditMember(int ID)
         {
             MemberBLL memberBLL = new MemberBLL();
-            Member member = memberBLL.GetByID(ID);
-            if (member == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                return View(member);
-            }
+            OrganizationBLL organizationBLL = new OrganizationBLL();
+            MemberOrganizationDTO memberOrganizationDTO = new MemberOrganizationDTO();
+            memberOrganizationDTO.Member = memberBLL.GetByID(ID);
+            memberOrganizationDTO.Organizations = organizationBLL.GetAll().ToList();
+            return View(memberOrganizationDTO);
         }
 
         [HttpPost]
@@ -71,31 +74,31 @@ namespace AidSystem.Controllers
             int Code,
             int OrganizationID)
         {
-            
-                if (FirstName == null)
-                {
-                    return ViewBag.Error = "Member don't exist";
-                }
-                else
-                {
-                    MemberBLL memberBLL = new MemberBLL();
-                    memberBLL.Update(
-                        ID,
-                        FirstName,
-                        SecondName,
-                        LastName,
-                        Code,
-                        OrganizationID);
-                }
-                return RedirectToAction("ViewAllMembers", "Member");
-        } 
-        
+
+            if (FirstName == null)
+            {
+                return ViewBag.Error = "Member don't exist";
+            }
+            else
+            {
+                MemberBLL memberBLL = new MemberBLL();
+                memberBLL.Update(
+                    ID,
+                    FirstName,
+                    SecondName,
+                    LastName,
+                    Code,
+                    OrganizationID);
+            }
+            return RedirectToAction("ViewAllMembers", "Member");
+        }
+
         public IActionResult Delete(int ID)
         {
             MemberBLL memberBLL = new MemberBLL();
             memberBLL.DeleteMember(ID);
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Dashboard", "Dashboard");
         }
     }
 }
